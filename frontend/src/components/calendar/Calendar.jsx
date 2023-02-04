1c5020450fded0a011897f4f715cde15bde04730
Wayne Zhu <waynezhu@Waynes-MacBook-Pro.local>
2023-02-04 12:35:02 -0800

    add timeline

--- frontend/src/components/calendar/Calendar.jsx
+++ frontend/src/components/calendar/Calendar.jsx
@@ -14,21 +14,19 @@ function App() {
   const { isLoading, getEpgProps, getLayoutProps } = useCalendar();
 
   return (
-    <div>
-      <div style={{ height: "80vh", width: "100%" }}>
-        <Epg isLoading={isLoading} {...getEpgProps()}>
-          <Layout
-            {...getLayoutProps()}
-            renderTimeline={(props) => <Timeline {...props} />}
-            renderProgram={({ program, ...rest }) => (
-              <ProgramItem key={program.data.id} program={program} {...rest} />
-            )}
-            renderChannel={({ channel }) => (
-              <ChannelItem key={channel.uuid} channel={channel} />
-            )}
-          />
-        </Epg>
-      </div>
+    <div className="calendar-main">
+      <Epg isLoading={isLoading} {...getEpgProps()}>
+        <Layout
+          {...getLayoutProps()}
+          renderTimeline={(props) => <Timeline {...props} />}
+          renderProgram={({ program, ...rest }) => (
+            <ProgramItem key={program.data.id} program={program} {...rest} />
+          )}
+          renderChannel={({ channel }) => (
+            <ChannelItem key={channel.uuid} channel={channel} />
+          )}
+        />
+      </Epg>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
-// import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,8 +17,3 @@ root.render(
     </Auth0Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
